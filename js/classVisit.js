741d5958edaaaa7abb2251ac4f2ff010b98618fc
Saveliy <saveliy.shaposhnik@gmail.com>
2021-01-28 22:15:32 +0200

    Visit and Modal

---
+++ js/classVisit.js
@@ -0,0 +1,32 @@
+class Visit {
+    constructor(options){
+        this.visitPurpose = options.visitPurpose;/*цель визита*/
+        this.shortDescription = options.shortDescription;/*краткое описание*/
+        this.urgency = options.urgency;/*срочность визитика*/
+        this.fullName = options.fullName;/*ФИО*/
+    }
+}
+
+class Cardiologist extends Visit{
+    constructor(options){
+        super(options);
+        this.normalPressure = options.normalPressure;/*обычное давление*/
+        this.bodyMassIndex = options.bodyMassIndex;/*индекс массы тела*/ 
+        this.pastDiseases = options.pastDiseases;/*перенесенные заболевания сердечно-сосудистой системы*/
+        this.age = options.age;/*возраст*/
+    }
+}
+
+class Dentist extends Visit{
+    constructor(options){
+        super(options);
+        this.lastVisit = options.lastVisit;/*дата последнего посещения*/
+    }
+}
+
+class Therapist extends Visit{
+    constructor(options){
+        super(options);
+        this.age = options.age;/*возраст*/
+    }
+}
